4c6f4d66ffb178d98944476ad0c0e6ac4e56919d
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-09 22:25:23 +0100

    [chore]:app api route setup

--- gateway/server.js
+++ gateway/server.js
@@ -9,8 +9,8 @@ app.use(express.json());
 
 
 app.use('/customer', proxy('http://localhost:8081'))
-app.use('/', proxy('http://localhost:8082'))
 app.use('/shopping', proxy('http://localhost:8083'))
+app.use('/', proxy('http://localhost:8082'))
 
 
 app.listen(8080, ()=>{
