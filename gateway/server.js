41a6e7067985b85d420455275b9375ca6cf2bca6
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-09 22:22:25 +0100

    [chore]:app api route setup

--- gateway/server.js
+++ gateway/server.js
@@ -8,6 +8,10 @@ app.use(cors());
 app.use(express.json());
 
 
+app.use('/customer', proxy('http://localhost:8081'))
+app.use('/', proxy('http://localhost:8082'))
+app.use('/shopping', proxy('http://localhost:8083'))
+
 
 app.listen(8080, ()=>{
     console.log("Gateway is listening on port 8080");
