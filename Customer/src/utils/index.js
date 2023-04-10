ecccb01aa6723484546aab77261f6534b1109d70
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-10 13:20:25 +0100

    [chore]: generate password

--- Customer/src/utils/index.js
+++ Customer/src/utils/index.js
@@ -8,3 +8,6 @@ module.exports.GenerateSalt = async () =>{
     return await bcrypt.genSalt();
 };
 
+module.exports.GeneratePassword = async (password, salt) =>{
+    return await bcrypt.hash(password, salt);
+};
