de2beb90fa34ae7b5b78763242ae20d132943227
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-10 13:19:02 +0100

    [chore]: generate salt

---
+++ Customer/src/utils/index.js
@@ -0,0 +1,10 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const {APP_SECRET} = require("../config/index");
+
+//utility functions
+module.exports.GenerateSalt = async () =>{
+    return await bcrypt.genSalt();
+};
+
