744a81fd4dc2c10c457be618fff052a9c962768d
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-10 13:07:23 +0100

    [chore]: error logger

--- Customer/src/utils/error-handler.js
+++ Customer/src/utils/error-handler.js
@@ -31,3 +31,41 @@ class ErrorLogger {
         }
     }
 }
+
+const ErrorHandler = async(err, req, res, next) =>{
+    const errorLogger = new ErrorLogger();
+
+    process.on('uncaughtException', (reason, promise) => {
+        console.log(reason, 'UNHANDLED');
+        throw reason; // need to take care
+    })
+
+    process.on('uncaughtException', (error) => {
+        errorLogger.logError(error);
+        if(errorLogger.isTrustError(err)){
+            //process exist // need restart
+        }
+    })
+    
+    // console.log(err.description, '-------> DESCRIPTION')
+    // console.log(err.message, '-------> MESSAGE')
+    // console.log(err.name, '-------> NAME')
+    if(err){
+        await errorLogger.logError(err);
+        if(errorLogger.isTrustError(err)){
+            if(err.errorStack){
+                const errorDescription = err.errorStack;
+                return res.status(err.statusCode).json({'message': errorDescription})
+            }
+            return res.status(err.statusCode).json({'message': err.message })
+        }else{
+            //process exit // terriablly wrong with flow need restart
+        }
+        return res.status(err.statusCode).json({'message': err.message})
+    }
+    next();
+    
+
+}
+
+module.exports = ErrorHandler;
