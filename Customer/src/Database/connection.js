3e48d93b2fc5461a4b93f73cad63614a5239d84d
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-10 08:56:59 +0100

    [chore]: database connection configurations

--- Customer/src/Database/connection.js
+++ Customer/src/Database/connection.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
-
+const {DB_URL} = require('../config/index')
 
 module.exports = async() => {
-    
+
+    try {
+        await mongoose.connect(DB_URL, {
+            useNewUrlParser: true,
+            useUnifieldTopology: true,
+            useCreateIndex: true
+        });
+        console.log("Db Connected Successfully");
+    } catch (error) {
+        console.log('Error ======');
+        console.log(error);
+        process.exit(1);
+    }
 }
