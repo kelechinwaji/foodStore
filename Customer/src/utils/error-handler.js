2dfa9e76e937a6c85d57ac32e8b7f647a946e59e
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-10 12:57:23 +0100

    [chore]: error logger

--- Customer/src/utils/error-handler.js
+++ Customer/src/utils/error-handler.js
@@ -7,3 +7,27 @@ const LogErrors = createLogger({
         new transports.File({filename: 'app_error.log'})
     ]
 });
+
+class ErrorLogger {
+    constructor(){}
+    async LogError(err){
+        console.log('==================== Start Error Logger ===============');
+        LogErrors.log({
+            private: true,
+            level: 'error',
+            message: `${new Date()}-${JSON.stringify(err)}`
+          });
+        console.log('==================== End Error Logger ===============');
+        // log error with Logger plugins
+      
+        return false;
+    }
+
+    isTrustError(error){
+        if(error instanceof AppError){
+            return error.isOperational;
+        } else {
+            return false;
+        }
+    }
+}
