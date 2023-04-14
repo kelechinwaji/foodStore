534b38e7bb6f8ed51f742d3520241c2fe00bbfd0
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-14 21:38:55 +0100

    [chore]: password salt generator

--- Product/src/utils/index.js
+++ Product/src/utils/index.js
@@ -2,3 +2,8 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const {APP_SECRET} = require("../config/index");
+
+//utility functions
+module.exports.GenerateSalt = async () =>{
+    return await bcrypt.genSalt();
+};
