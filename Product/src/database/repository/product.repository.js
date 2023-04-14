8aded61546cde1569372ff271b269992648a212a
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-14 21:47:11 +0100

    [chore]: create product repostory

--- Product/src/database/repository/product.repository.js
+++ Product/src/database/repository/product.repository.js
@@ -3,5 +3,36 @@ const {ApiError, BadRequestError} = require("../../utils/app.error")
 
 //Database Operations
 class ProductRepository {
+    async CreateProduct({
+        name,
+        desc,
+        type,
+        unit,
+        price,
+        available,
+        suplier,
+        banner,
+      }) {
+        try {
+          const product = new ProductModel({
+            name,
+            desc,
+            type,
+            unit,
+            price,
+            available,
+            suplier,
+            banner,
+          });
     
+          const productResult = await product.save();
+          return productResult;
+        } catch (err) {
+          throw new ApiError(
+            "API Error",
+            STATUS_CODES.INTERNAL_ERROR,
+            "Unable to Create Product"
+          );
+        }
+      }
 }
