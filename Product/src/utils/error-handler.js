4ac5876c3777381c87918e55ed22fa73be901fb6
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-14 21:36:22 +0100

    [chore]: error logger

---
+++ Product/src/utils/error-handler.js
@@ -0,0 +1,9 @@
+const {createLogger, transport, transports} = require('winston');
+const {AppError} = require('./app-errors')
+
+const LogErrors = createLogger({
+    transports: [
+        new transports.Console(),
+        new transports.File({filename: 'app_error.log'})
+    ]
+});
