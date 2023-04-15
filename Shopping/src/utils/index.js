a071d3627f256d8c0f8e970d7ba686de960827b2
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-15 22:30:37 +0100

    [chore] password validation

--- Shopping/src/utils/index.js
+++ Shopping/src/utils/index.js
@@ -19,3 +19,12 @@ module.exports.validatePassword = async (
 )=>{
     return (await this.GeneratePassword(enteredPassword, salt)) === savePassword
 };
+
+module.exports.GenerateSignature = async (payload) =>{
+    try {
+        return await jwt.sign(payload, APP_SECRET, {expiresIn: "10d"});
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
