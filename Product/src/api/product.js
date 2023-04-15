a2c03465c1e19e1705c32fca0ad7943b74ef2f1c
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-15 17:47:41 +0100

    [feat]:product api route

--- Product/src/api/product.js
+++ Product/src/api/product.js
@@ -1,6 +1,7 @@
 const ProductController = require("../controller/product.controller");
 const UserAuth = require("../controller/middleware/auth")
 
-module.exports = (qpp) => {
-   
+module.exports = (app) => {
+    const product = new ProductController();
+   app.post('/Product/create', product.create)
 }
