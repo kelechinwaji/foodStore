b9b41b00a520729f10562a0ef7ad2e0e1a9e41dd
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-15 22:36:08 +0100

    [chore] shopping database repository

--- Shopping/src/database/repository/shoping.repository.js
+++ Shopping/src/database/repository/shoping.repository.js
@@ -1,3 +1,17 @@
 const orderModels = require("../models/order.models");
 const { v4: uuidv4 } = require('uuid');
+const {ApiError, BadRequestError} = require("../../utils/app.error")
 
+// Database operations
+
+class ShoppingRepository {
+    // payment
+    async Orders(customerId){
+        try{
+            const orders = await OrderModel.find({customerId }).populate('items.product');        
+            return orders;
+        }catch(err){
+            throw ApiError('API Error', STATUS_CODES.INTERNAL_ERROR, 'Unable to Find Orders')
+        }
+    }
+}
