0e4c32a9855119b8022667ae81cbbef88863a6eb
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-15 17:43:19 +0100

    [feat]: customer api route

--- Customer/src/api/customer.js
+++ Customer/src/api/customer.js
@@ -1,7 +1,7 @@
 const CustomerController = require("../controller/customer.controller")
 const UserAuth = require("./middleware/auth")
 
-module.exports = (qpp) => {
+module.exports = (app) => {
     const customer = new CustomerController();
     app.post("/customer/signup", customer.signUp);
     app.post("/customer/signin", customer.login);
