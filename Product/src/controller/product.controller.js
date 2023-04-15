2cb40fb6a919c481bd116c4304c4e3202a835fd3
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-15 17:16:31 +0100

    [feat]:create product

---
+++ Product/src/controller/product.controller.js
@@ -0,0 +1,16 @@
+const ProductService = require("../service/product.service");
+const UserAuth = require("../controller/middleware/auth");
+
+const service = new ProductService();
+
+class ProductController {
+   async create(req, res){
+    try {
+        const {name, desc, type, unit, price, available, suplier, banner} = req.body
+        const result = await service.CreateProduct({name, desc, type, unit, price, available, suplier, banner});
+        return res.json({status: true, data: result})
+    } catch (error) {
+        return res.json({status: true, data: error})
+    }
+   } 
+}
