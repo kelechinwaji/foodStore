8f1823f78e317b671c4ae6351b89a93a66f82f60
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-16 22:35:59 +0100

    [chore] micro service setup

--- Shopping/src/database/connection.js
+++ Shopping/src/database/connection.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
-const { DB_URL } = require('../config');
+// const { DB_URL } = require('../config');
+const dotEnv = require('dotenv');
 
+dotEnv.config()
+const DB_URL = process.env.MONGODB_URI;
 module.exports = async() => {
 
     try {
         await mongoose.connect(DB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true
+            
         });
         console.log('Db Connected');
         
